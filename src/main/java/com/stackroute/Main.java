9ed71578e8b2d6eea11719530c2859cdc0378d27
madhurih <hiremadhuri66@gmail.com>
2019-05-24 16:40:31 +0530

    Task 4 completed

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -17,29 +17,28 @@ public class Main
 {
     public static void main( String[] args )
     {
-        Resource resource = new ClassPathResource("beans.xml");
-        BeanFactory factory = new XmlBeanFactory(resource);
-        Movie movie = (Movie) factory.getBean("movie");
-        System.out.println(movie.getActor().getName()+ " acted in: " + movie.getMovieName());
+//        Resource resource = new ClassPathResource("beans.xml");
+//        BeanFactory factory = new XmlBeanFactory(resource);
+//        Movie movie = (Movie) factory.getBean("movie");
+//        System.out.println(movie.getActor().getName()+ " acted in: " + movie.getMovieName());
 
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         Movie movie1 = (Movie) context.getBean("movie1");
         System.out.println(movie1.getActor().getName()+ " acted in: " + movie1.getMovieName());
 
+//        BeanDefinitionRegistry beanDefinitionRegistry = new DefaultListableBeanFactory();
+//        BeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanDefinitionRegistry);
+//        beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
+//        Movie movie2=(Movie) ((DefaultListableBeanFactory) beanDefinitionRegistry).getBean("movie2");
+//        System.out.println(movie2.getActor().getName()+ " acted in: " + movie2.getMovieName());
 
-        BeanDefinitionRegistry beanDefinitionRegistry = new DefaultListableBeanFactory();
-        BeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanDefinitionRegistry);
-        beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
-        Movie movie2=(Movie) ((DefaultListableBeanFactory) beanDefinitionRegistry).getBean("movie2");
-        System.out.println(movie2.getActor().getName()+ " acted in: " + movie2.getMovieName());
-
-        //testing bean scope
-        ApplicationContext context1 = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie3 = (Movie) context1.getBean("movie");
-
-        Movie movie4 = (Movie) context1.getBean("movie");
-
-        System.out.println(movie3 == movie4);
+//        //testing bean scope
+//        ApplicationContext context1 = new ClassPathXmlApplicationContext("beans.xml");
+//        Movie movie3 = (Movie) context1.getBean("movie");
+//
+//        Movie movie4 = (Movie) context1.getBean("movie");
+//
+//        System.out.println(movie3 == movie4);
 
     }
 }
--- src/main/java/com/stackroute/awareInterface/ResourceLoaderAwareDemo.java
+++ src/main/java/com/stackroute/awareInterface/ResourceLoaderAwareDemo.java
@@ -1,4 +1,31 @@
 package com.stackroute.awareInterface;
 
-public class ResourceLoaderAwareDemo {
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class ResourceLoaderAwareDemo implements ResourceLoaderAware {
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        Resource resource = resourceLoader.getResource("data.txt");
+
+        try{
+            InputStream is = resource.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+            br.close();
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 }
