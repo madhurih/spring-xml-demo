a3ee50dc33162d244413fd1de12f8153d262733d
madhurih <hiremadhuri66@gmail.com>
2019-05-24 11:34:04 +0530

    added code to test scope

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -6,31 +6,40 @@ import org.springframework.beans.factory.support.BeanDefinitionReader;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 
 public class Main
 {
     public static void main( String[] args )
     {
-//        Resource resource = new ClassPathResource("beans.xml");
-//        BeanFactory factory = new XmlBeanFactory(resource);
-//        Movie movie = (Movie) factory.getBean("movie");
-//        System.out.println(movie.getActor().getName()+ " acted in: " + movie.getMovieName());
-
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie = (Movie) context.getBean("movie");
+        Resource resource = new ClassPathResource("beans.xml");
+        BeanFactory factory = new XmlBeanFactory(resource);
+        Movie movie = (Movie) factory.getBean("movie");
         System.out.println(movie.getActor().getName()+ " acted in: " + movie.getMovieName());
 
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Movie movie1 = (Movie) context.getBean("movie");
+        System.out.println(movie1.getActor().getName()+ " acted in: " + movie1.getMovieName());
 
-//        BeanDefinitionRegistry beanDefinitionRegistry = new DefaultListableBeanFactory();
-//        BeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanDefinitionRegistry);
-//        beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
-//        Movie movie=(Movie) ((DefaultListableBeanFactory) beanDefinitionRegistry).getBean("movie");
-//        System.out.println(movie.getActor().getName()+ " acted in: " + movie.getMovieName());
 
+        BeanDefinitionRegistry beanDefinitionRegistry = new DefaultListableBeanFactory();
+        BeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanDefinitionRegistry);
+        beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
+        Movie movie2=(Movie) ((DefaultListableBeanFactory) beanDefinitionRegistry).getBean("movie");
+        System.out.println(movie2.getActor().getName()+ " acted in: " + movie2.getMovieName());
+
+        //testing bean scope
+        ApplicationContext context1 = new ClassPathXmlApplicationContext("beans.xml");
+        Movie movie3 = (Movie) context.getBean("movie");
+
+        Movie movie4 = (Movie) context.getBean("movie");
+
+        System.out.println(movie3 == movie4);
 
     }
 }
